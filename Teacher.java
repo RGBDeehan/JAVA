671d60474204749a83ad52eaf4fef01b55ea822d
Rageeb Yasar Deehan <rageebyasardeehan@gmail.com>
2025-04-28 23:55:29 +0600

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,26 +1,24 @@
 public class Teacher extends User {
-    private String designation;
-    private static final int MAX_BOOKS = 5;
-    
-    public Teacher(String userId, String name, String designation) {
-        super(userId, name);
+    String designation;
+    int maxBooks =5;
+
+    Teacher(int userId, String name, String designation) {
+        super(name,userId);
         this.designation = designation;
     }
-    
-    @Override
-    public boolean borrowBook(Book book) {
-        if (borrowedBooks.size() >= MAX_BOOKS) {
-            System.out.println("You've reached the maximum limit of " + MAX_BOOKS + " books.");
-            return false;
+    public void borrowBook(Book book, int availablecopies) {
+        if (borrowedBooks.size() >= maxBooks) {
+            System.out.println("You've reached the maximum limit of " + maxBooks + " books.");
         }
-        if (book.borrow()) {
+        book.borrow(availablecopies);
             borrowedBooks.add(book);
-            System.out.println("Book borrowed successfully.");
-            return true;
+        System.out.println("Book borrowed successfully.");
         }
-        System.out.println("Book not available.");
-        return false;
+
+
+    @Override
+    void borrowBook(Book book) {
+
     }
-    
-    public String getDesignation() { return designation; }
 }
+
